be19bb07cf28aecef785acce4a0dc7aa92a4bbf7
helixlab <lennonbfr@gmail.com>
2026-03-28 22:33:03 -0300

    Update script_manicure.py

--- script_manicure.py
+++ script_manicure.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 # ---------------------------------------------------
 # CONFIGURAÇÃO
@@ -11,6 +12,36 @@ st.set_page_config(
 )
 
 LINK_VENDAS_MANICURE = "https://go.hotmart.com/Y104886121U"
+PIXEL_ID = "1759073455503460"
+
+# ---------------------------------------------------
+# PIXEL META
+# ---------------------------------------------------
+
+pixel_code = f"""
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{{if(f.fbq)return;n=f.fbq=function(){{n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)}};
+if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '{PIXEL_ID}');
+fbq('track', 'PageView');
+</script>
+
+<noscript>
+<img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id={PIXEL_ID}&ev=PageView&noscript=1"/>
+</noscript>
+<!-- End Meta Pixel Code -->
+"""
+components.html(pixel_code, height=0)
 
 # ---------------------------------------------------
 # CSS
@@ -95,18 +126,22 @@ Esse treinamento foi pensado para manicures que querem:
 """, unsafe_allow_html=True)
 
 st.write("")
-
 st.write("Clique abaixo para ver como funciona o treinamento completo.")
 
 # ---------------------------------------------------
-# BOTÃO DIRETO PARA OFERTA
+# BOTÃO COM EVENTO + REDIRECIONAMENTO
 # ---------------------------------------------------
 
-st.link_button(
-    "👉 VER TREINAMENTO AGORA",
-    LINK_VENDAS_MANICURE,
-    use_container_width=True
-)
+if st.button("👉 VER TREINAMENTO AGORA", use_container_width=True):
+    redirect_code = f"""
+    <script>
+        if (typeof fbq !== 'undefined') {{
+            fbq('track', 'ViewContent');
+        }}
+        window.open('{LINK_VENDAS_MANICURE}', '_self');
+    </script>
+    """
+    components.html(redirect_code, height=0)
 
 # ---------------------------------------------------
 
